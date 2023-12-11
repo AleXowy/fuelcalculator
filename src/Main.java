f01755f013bd159769b0394c415854c7cc450827
AleXowy <alexowy97@gmail.com>
2023-12-11 18:01:27 +0000

    Kalkulator paliwa v2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 
-import java.sql.SQLOutput;
+import javax.swing.*;
+import java.math.BigDecimal;
+
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,40 +13,71 @@ public class Main {
 
 
 
+
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.ENGLISH);
         System.out.println("Podaj czas wyścigu");
 
-        double raceTime = scanner.nextInt();
-
+        double raceTime = scanner.nextDouble();
 
         double raceTimesec = raceTime * 60;
 
 
-        System.out.println("Podaj czas okrążenia MM.SS");
+        System.out.println("Podaj czas okrążenia MM.SS.MM");
         String imputlaptime = scanner.next();
 
-        String[] timeParts = imputlaptime.split("[. :]");
+        String[] timeParts = imputlaptime.split("[. :,]");
 
-        if (timeParts.length != 2) {
-            System.out.println("źle");
-            return;
+
+
+
+
+
+
+        double lapTimeMin = Double.parseDouble(timeParts[0]);
+        double lapTimeSec = Double.parseDouble(timeParts[1]);
+        double lapTimeMm;
+
+
+        if (timeParts.length != 3) {
+            lapTimeMm = 0;
+
+        }else {
+            lapTimeMin = Double.parseDouble(timeParts[0]);
+            lapTimeSec = Double.parseDouble(timeParts[1]);
+            lapTimeMm = Double.parseDouble(timeParts[2]);
+
+
         }
 
-        double lapTimeMin = Integer.parseInt(timeParts[0]);
-        double lapTimeSec = Integer.parseInt(timeParts[1]);
+        double lapTimeSecTotal = (lapTimeMin * 60 + lapTimeSec) + (lapTimeMm / 10);
 
-        double lapTimeSecTotal = lapTimeMin * 60 + lapTimeSec;
-
-        double raceLaps = (double) raceTimesec / lapTimeSecTotal;
+        double raceLaps = raceTimesec / lapTimeSecTotal;
 
         System.out.println("Podaj spalanie");
         double fuelPerLap = scanner.nextDouble();
 
         double raceFuel = (raceLaps * fuelPerLap);
 
-        System.out.println(raceFuel);
+        double raceLapsPlus1 = (raceTimesec / lapTimeSecTotal);
 
+        double raceFuelPlus1 = (raceLapsPlus1 * fuelPerLap)+fuelPerLap;
+
+
+
+
+        int decimalPlaces = 0;
+        BigDecimal raceLapsPlus1rounded = BigDecimal.valueOf(raceFuelPlus1).setScale(decimalPlaces,BigDecimal.ROUND_UP);
+        BigDecimal raceFuelRounded = BigDecimal.valueOf(raceFuel).setScale(decimalPlaces,BigDecimal.ROUND_UP);
+        BigDecimal racelapsRounded = BigDecimal.valueOf(raceLaps).setScale(decimalPlaces,BigDecimal.ROUND_UP);
+
+
+
+
+
+        System.out.println("Minimalna Ilość paliwa to: " + raceFuelRounded);
+        System.out.println("Ilość Lapów: " + racelapsRounded);
+        System.out.println("Ilość paliwa +1 lap: " + raceLapsPlus1rounded);
 
 
 
